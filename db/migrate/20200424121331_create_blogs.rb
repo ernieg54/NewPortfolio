f4ba5624a2d4c72ebbb8a99fcaad215d7dfb41bc
Ernie Gibbs <egibbs@bigpond.com>
2020-05-07 18:40:55 +1000

    Fixed blogs status not working as intended

--- db/migrate/20200424121331_create_blogs.rb
+++ db/migrate/20200424121331_create_blogs.rb
@@ -3,7 +3,7 @@ class CreateBlogs < ActiveRecord::Migration[6.0]
     create_table :blogs do |t|
       t.string :title
       t.text :body
-
+      t.integer :status default => 0
       t.timestamps
     end
   end
