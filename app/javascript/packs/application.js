8d28ece0ef08de931c20267a94e4cb0b61dc7b5b
Ernie Gibbs <egibbs@bigpond.com>
2020-06-15 19:27:59 +1000

    Installed jquery ui, sortable ui html5 library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,17 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+require("jquery-ui").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("packs")
 
 import "bootstrap"
+
+// Default SortableJS
+import Sortable from 'sortablejs';
+
+require("turbolinks").start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
